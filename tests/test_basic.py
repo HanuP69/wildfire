b176c85f1f1120f48bc4e66bf6c44379815d1622
HanuP69 <honeypandey680@gmail.com>
2026-03-13 01:32:19 +0530

    lock docker fixes

---
+++ tests/test_basic.py
@@ -0,0 +1,12 @@
+import pytest
+
+def test_environment():
+    """Basic test to verify the testing environment is set up correctly."""
+    assert True
+
+def test_imports():
+    """Verify that core packages can be imported."""
+    import torch
+    import pandas
+    import numpy
+    assert True
